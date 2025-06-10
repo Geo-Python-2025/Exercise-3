654a560c8f7b0d317efc21caf129aa077eaee632
mikkokangasmaa <90315571+mikkokangasmaa@users.noreply.github.com>
2025-06-10 10:29:30 +0300

    Update test_exercise_3_problem_1.py

--- .tests/test_exercise_3_problem_1.py
+++ .tests/test_exercise_3_problem_1.py
@@ -2,7 +2,7 @@
 from points_decorator import points
 
 class TestProblem1:
-    @points(0.25, "Part 1: Variable 'basename' is not correctly defined!")
+    @points(0.25, "Problem 1, Part 1: Variable 'basename' is not correctly defined!")
     def test_problem_1_part_1_basename(self, problem1):
         section_data, namespace = problem1
         section = "Part 1" 
@@ -10,7 +10,7 @@ class TestProblem1:
 
         assert variables['basename'] == "Station"
 
-    @points(0.25, "Part 1: Variable 'filenames' is not an empty list!")
+    @points(0.25, "Problem 1, Part 1: Variable 'filenames' is not an empty list!")
     def test_problem_1_part_1_filenames(self, problem1):
         section_data, namespace = problem1
         section = "Part 1"  
@@ -18,7 +18,7 @@ class TestProblem1:
 
         assert variables['filenames'] == []
 
-    @points(0.75, "Part 2: Loop not found in source code!")
+    @points(0.75, "Problem 1, Part 2: Loop not found in source code!")
     def test_problem_1_part_2_loop(self, problem1):
         section_data, namespace = problem1
         section = "Part 2"  
@@ -26,7 +26,7 @@ class TestProblem1:
 
         assert any(loop in source for loop in ["for", "while"]), "Loop not found in source code!"
 
-    @points(0.75, "Part 2: Filenames list length is not correct!")
+    @points(0.75, "Problem 1, Part 2: Filenames list length is not correct!")
     def test_problem_1_part_2_filenames_length(self, problem1):
         section_data, namespace = problem1
         section = "Part 2"  
@@ -34,7 +34,7 @@ class TestProblem1:
 
         assert len(variables['filenames']) == 21
 
-    @points(0.5, "Part 2: Last item in filenames list not formatted correctly!")
+    @points(0.5, "Problem 1, Part 2: Last item in filenames list not formatted correctly!")
     def test_problem_1_part_2_filenames_last_item(self, problem1):
         section_data, namespace = problem1
         section = "Part 2"   
